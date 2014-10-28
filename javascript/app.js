b67d52b434156394cabc2560d1d73a46cdc9487f
Alejandro <alez.hdz.88@gmail.com>
2014-10-27 23:55:38 -0600

    Se agrego el ejemplo watch

--- javascript/app.js
+++ javascript/app.js
@@ -10,4 +10,19 @@ angular.module('app', [])
 '$scope', 
 function($scope){
 	$scope.hello = "hello world";
+}])
+.controller('StartUpController', [
+'$scope', 
+function($scope){
+
+	$scope.funding = {
+		startingEstimate: 0
+	};
+
+	computeNeeded = function(){
+		$scope.funding.needed = $scope.funding.startingEstimate * 10;
+	};
+
+	$scope.$watch('funding.startingEstimate', computeNeeded);
+
 }]);
